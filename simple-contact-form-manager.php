f93d3d878ecf242547598b6ea1ae9c27615a4f0a
devfardin <contactfardin22@gmail.com>
2026-01-13 01:15:37 +0600

    create plugin file

---
+++ simple-contact-form-manager.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Plugin Name: Sinple Contact Form Manager
+ * Description: It's a Simple Contact form Manager Plugin, that will be help to collect information from clients
+ * Tags: Contact From, Simple Contact Form
+ * Plugin URI: https://simple-contact-form-management.com
+ * Version: 1.0.0
+ * Author Name: Fardin
+ * Author URI: https://github.com/devfardin
+ * Text Domain: scfm
+ */
